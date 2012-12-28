aeb7b72fe640c958808395061010fe0ebf1eb3aa
Cory Benfield <lukasaoz@gmail.com>
2012-12-28 12:04:59 +0000

    Add functionality to strip auth.

---
+++ requests_ftp/__init__.py
@@ -0,0 +1,1 @@
+from .ftp import FTPAdapter
--- requests_ftp/ftp.py
+++ requests_ftp/ftp.py
@@ -1,5 +1,7 @@
 # -*- encoding: utf-8 -*-
 from requests.adapters import BaseAdapter
+import ftplib
+import base64
 
 
 class FTPAdapter(BaseAdapter):
@@ -7,7 +9,10 @@ class FTPAdapter(BaseAdapter):
     def __init__(self):
         super(FTPAdapter, self).__init__()
 
-    def send(self):
+        # Currently there's no connection pooling here, so we don't need to
+        # maintain any state.
+
+    def send(self, request, **kwargs):
         '''Sends a PreparedRequest object over FTP. Returns a response object.
         '''
         raise NotImplementedError('Not yet implemented.')
@@ -15,3 +20,37 @@ class FTPAdapter(BaseAdapter):
     def close(self):
         '''Dispose of any internal state.'''
         raise NotImplementedError('Not yet implemented.')
+
+    def get_username_password_from_header(self, request):
+        '''Given a PreparedRequest object, reverse the process of adding HTTP
+        Basic auth to obtain the username and password. Allows the FTP adapter
+        to piggyback on the basic auth notation without changing the control
+        flow.'''
+        auth_header = request.headers.get('Authorization')
+
+        if auth_header:
+            # The basic auth header is of the form 'Basic xyz'. We want the
+            # second part. Check that we have the right kind of auth though.
+            encoded_components = auth_header.split()[:2]
+            if encoded_components[0] != 'Basic':
+                raise AuthError('Invalid form of Authentication used.')
+            else:
+                encoded = encoded_components[1]
+
+            # Decode the base64 encoded string.
+            decoded = base64.b64decode(encoded)
+
+            # The string is of the form 'username:password'. Split on the
+            # colon.
+            components = decoded.split(':')
+            username = components[0]
+            password = components[1]
+            return (username, password)
+        else:
+            # No auth header. Return None.
+            return None
+
+
+class AuthError(Exception):
+    '''Denotes an error with authentication.'''
+    pass
